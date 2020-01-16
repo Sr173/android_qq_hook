a9c91dcd0e85fe78c2e77bd4d8282d1b189d40ce
sr173 <admin@DESKTOP-3S7BRR6>
2020-01-16 23:58:15 +0800

    demo

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import tkinter
 
+script = 0
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print("[*] {0}".format(message['payload']))
+    else:
+        print(message)
+
 def button_call_back():
-    file_object = open('run.js', encoding="utf-8")
+    if script != 0:
+        script.unload()
+    file_object = open('hook.js', encoding="utf-8")
     text = file_object.read()
     file_object.close()
+    device = frida.get_usb_device()
+    process = device.attach("com.tencent.mobileqq:MSF")
+    script = process.create_script(text)
+    script.on('message', on_message)
+    script.load()
 
 top = tkinter.Tk()
 button = tkinter.Button(top, text="执行脚本", command=button_call_back)
